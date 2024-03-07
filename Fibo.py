491296ed4acf7c4ffab56192da457d0435d0cd37
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2024-03-06 18:23:28 -0600

    Mathematical Functions General Project
    This program generates a series of Fibonacci numbers and saves it in lists and dictionaries, in such a way that it then makes breakdowns and dumps of the intermediate values that were generated to be able to generate it.

---
+++ Fibo.py
@@ -0,0 +1,67 @@
+# Fibonacci series from 'x' and 'y' up to 'n'.
+LIM_MIN = 0
+LIM_MAX = 45
+
+dictFibo = {}
+listFibo = []
+
+# Fibonacci function.
+def fib(n, x, y):
+	print()
+	print("Generation of the Fibonacci series.")
+
+	for i in range(0, n):
+		print(f"#[{i}]\t:\t[{x}]\t+\t[{y}]\t=\t[{x+y}].")
+
+		dictFibo[i] = [x, y, x+y]
+		listFibo.append([x, y, x+y])
+
+		x, y = y, x+y
+
+	print()
+	return y
+
+# Main procedure.
+print("+---|----+---|----+---|----+---|")
+print("| Fibonacci's series Generator.|")
+print("+---|----+---|----+---|----+---|")
+n = int(input(f"Number of series from [{LIM_MIN}] to [{LIM_MAX}]: "))
+
+if n>=LIM_MIN and n<=LIM_MAX:
+	x = int(input("First number  : "))
+	y = int(input("Second number : "))
+	fib = fib(n, x, y)
+
+	print()
+	print(f"Latest Fibonacci result for [{x}] and [{y}] is: [{fib}] with [{n}] iterations.")
+
+	print()
+	print("List containing lists of the Fibonacci series.")
+	print(listFibo)
+
+	print()
+	print("Dictionary containing lists of the Fibonacci series.")
+	print(dictFibo)
+
+	print()
+	print("Dictionary breakdown of the Fibonacci series.")
+	for key in dictFibo:
+		print(f"#[{key}] :", end='\t')
+
+		for value in dictFibo[key]:
+			print(f"[{value}].", end='\t')
+
+		print()
+
+	print()
+	print("Dictionary dump of the Fibonacci series.")
+	for key, row in dictFibo.items():
+		print(f"#[{key}] :", end='\t')
+
+		for value in row:
+			print(f"[{value}].", end='\t')
+
+		print()
+
+else:
+	print(f"Error! The value: [{n}] is not between [{LIM_MIN}] and [{LIM_MAX}].")
