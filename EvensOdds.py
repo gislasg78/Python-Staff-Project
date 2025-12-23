4159397179fb76f5ff318ff44586376b8e311b82
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2025-12-23 17:37:36 -0600

    Add files via upload
    Mathematical Functions General Project
    The purpose of this program is to separate the even, odd and prime elements from a list of integers and return in one function a list of lists with the count and sum data of each type, and another function that returns the list of elements separated into even, odd and prime for ease of use.

--- EvensOdds.py
+++ EvensOdds.py
@@ -4,9 +4,19 @@ def IsEven(number):
 def IsOdd(number):
 	return (number % 2)
 
-def sum_of_evens_and_odds(numbers):
+def IsPrime(number):
+	return (((number == 2) or (number == 3) or (number == 5) or (number == 7)) or
+		((number % 2) and (number % 3) and (number % 5) and (number % 7)))
+
+def extract_of_evens_odds_primes(numbers):
+	return [sorted([x for x in numbers if IsEven(x)]),
+		sorted([x for x in numbers if IsOdd(x)]),
+		sorted([x for x in numbers if IsPrime(x)])]
+
+def sum_of_evens_odds_primes(numbers):
 	addition_evens, counter_evens = 0, 0
 	addition_odds, counter_odds = 0, 0
+	addition_primes, counter_primes = 0, 0
 
 	for number in numbers:
 		if (IsEven(number)):
@@ -17,50 +27,53 @@ def sum_of_evens_and_odds(numbers):
 			counter_odds += 1
 			addition_odds += number
 
-	return [[counter_evens, addition_evens], [counter_odds, addition_odds]]
+		if (IsPrime(number)):
+			counter_primes += 1
+			addition_primes += number
 
-def extract_of_evens_and_odds(numbers):
-	return [sorted([x for x in numbers if IsEven(x)]),
-		sorted([x for x in numbers if IsOdd(x)])]
+	return [[counter_evens, addition_evens],
+		[counter_odds, addition_odds],
+		[counter_primes, addition_primes]]
 
-legends = [["Evens", "Odds"], ["Count", "Sum"]]
+legends = [["Evens", "Odds", "Primes"], ["Count", "Sum"]]
 numbers = [62, 66, 94, 97, 25, 11, 68, 54, 48, 67]
-extract = extract_of_evens_and_odds(numbers)
-result = sum_of_evens_and_odds(numbers)
 
-print("Sum of even and odd numbers exercise.")
-print("+ List:")
-print(f"{numbers}")
+extracts = extract_of_evens_odds_primes(numbers)
+summations = sum_of_evens_odds_primes(numbers)
 
-print()
-print("Summatories.")
+print("Sum of even, odd and prime numbers exercise.")
+print(f"+ List: {sorted(numbers)}")
 
-counter = 0
-for row in result:
-	print(f"{legends[0][counter]}.")
-	counter += 1
-
-	count = 0
-	for stall in row:
-		print(f"+ {legends[1][count]} : [{stall}].")
-		count += 1
+first_header, second_header = 0, 1
 
 print()
 print("Extractions.")
 
 counter = 0
-for row in extract:
-	print(f"{legends[0][counter]}.")
+for row in extracts:
+	print(f"{legends[first_header][counter]}.")
 	print(f"+ Count : [{len(row)}].")
 	print(f"+ Sum : [{sum(row)}].")
 	counter += 1
 
 	for stall in row:
-		print(f"{stall}", end='\t')
+		print(f"({stall})", end='\t')
 
 	print("\n")
 
+print("Summatories.")
+
+counter = 0
+for row in summations:
+	print(f"{legends[first_header][counter]}.")
+	counter += 1
+
+	count = 0
+	for stall in row:
+		print(f"+ {legends[second_header][count]} : [{stall}].")
+		count += 1
+
 print()
 print("Results.")
-print(f"{result}")
-print(f"{extract}")
+print(f"{extracts}")
+print(f"{summations}")
