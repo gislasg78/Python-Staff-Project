f27b36802cd89894d758b9c88674ee37214e862a
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2026-01-02 10:12:27 -0600

    Add files via upload
    Mathematical Functions General Project
    This program aims to demonstrate an exercise in guessing a number within a given range, usually between 1 and 100, and to deduce during program execution whether the guessed number is lower or higher than the hidden number. The number of attempts is counted, and each attempt is saved respectly in a list, in a dictionary and in a set. If the player gives up after guessing a number outside the established range, the results obtained with the number to be guessed are displayed on the screen with its frecuency of each captured element.

--- GuessNumber.py
+++ GuessNumber.py
@@ -7,7 +7,10 @@ correct_number = random.randint(lower_limit, upper_limit)
 factor = random.random()
 guess = random.randrange(lower_limit, upper_limit)
 guess_count = 0
+
 list_of_guesses = list()
+dict_of_guesses = {}
+set_of_guesses = set()
 
 print("Hi! Welcome to the guessing game.")
 print(f"Please guess a number between: [{lower_limit}] and: [{upper_limit}].")
@@ -17,16 +20,24 @@ print("Type zero to give up!")
 print()
 
 while ((guess >= lower_limit) and (guess <= upper_limit)) and guess != correct_number and guess != 0:
-	print(f"Tried \x23: [{guess_count + 1}].")
+	print(f"Tried\x20\x23: [{guess_count + 1}].")
 	print("Enter zero to exit.")
 
 	try:
 		guess = int(input(f"What is your guest ({lower_limit} - {upper_limit})? : "))
+
+		list_of_guesses.append(guess)
+
+		if guess in dict_of_guesses:
+			dict_of_guesses[guess] += 1
+		else:
+			dict_of_guesses[guess] = 1
+
+		set_of_guesses.add(guess)
+
 	except Exception as e:
 		guess = 0
 		print(e)
-
-	list_of_guesses.append(guess)
 
 	if guess > correct_number:
 		print(f"Wrong! You need to guess lower.\n")
@@ -47,5 +58,11 @@ else:
 	print("You failed!")
 	print(f"Your last number: [{guess}] does not match the guessed number: [{correct_number}].")
 
-print(f"Numbers guessed: {list_of_guesses}.")
+print()
+print("Numbers guessed:")
+print(f"{list_of_guesses}")
+print(f"{dict_of_guesses}")
+print(f"{set_of_guesses}")
+
+print()
 print(f"It took you: [{guess_count}] guesses.")
