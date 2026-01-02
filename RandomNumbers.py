f733b379c7a723220f08cc234112274677fffabb
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2026-01-02 10:55:20 -0600

    Mathematical Functions General Project
    This simple program aims to generate a certain number of tickets for the Melate lottery using Python's default Random Number Generator, and returns them sorted and in the form of a list of lists.

--- RandomNumbers.py
+++ RandomNumbers.py
@@ -4,19 +4,29 @@ from datetime import datetime
 
 # Random Ticket Function.
 def ticket(nums_by_ticket, minimum, maximum):
-	return list(sorted(random.sample(range(minimum, maximum), nums_by_ticket)))
+	try:
+		return list(sorted(random.sample(range(minimum, maximum), nums_by_ticket)))
+	except Exception as e:
+		print(e)
+		return list()
 
 # Random Tickets Generator.
 def tickets(quantity_tickets, nums_by_ticket, minimum, maximum):
 	myTickets = []
 
 	for i in range(0, quantity_tickets):
-		print(f"\nTicket: [{i + 1}] of: [{quantity_tickets}].")
+		print(f"\nTicket\x20\x23\x3a [{i + 1}] of: [{quantity_tickets}]\x2e")
 
 		for j in range(0, nums_by_ticket):
-			lotteryWinners = ticket(nums_by_ticket, minimum, maximum)
-			print(f"#: {j + 1} = {lotteryWinners}.")
-			myTickets.append(lotteryWinners)
+			try:
+				lotteryWinners = ticket(nums_by_ticket, minimum, maximum)
+
+				if lotteryWinners:
+					print(f"\x23\x3a {j + 1} \x3d {lotteryWinners}\x2e")
+					myTickets.append(lotteryWinners)
+
+			except Exception as e:
+				print(e)
 
 	return myTickets
 
@@ -35,16 +45,26 @@ print("You rolled a: [" + str(random.randrange(1, 7)) + "].")
 # Random Choices.
 print()
 print("Random number generation for Lottery.")
-quantity_tickets = int(input("Number of tickets: "))
-numbers_by_ticket = int(input("Numbers per ticket: "))
-minimum_value = int(input("Minimum value: "))
-maximum_value = int(input("Maximum value: "))
+
+try:
+	quantity_tickets = int(input("Number of tickets: "))
+	numbers_by_ticket = int(input("Numbers per ticket: "))
+	minimum_value = int(input("Minimum value: "))
+	maximum_value = int(input("Maximum value: "))
+except Exception as e:
+	print(e)
+	quantity_tickets, numbers_by_tickets, minimum_value, maximum_value = 0, 0, 0, 0
 
 print()
 print("List of randomly generated tickets.")
 print(datetime.now().strftime("[%A, %B %d, %Y] - [%H:%M:%S]."))
 
-list_of_tickets = tickets(quantity_tickets, numbers_by_ticket, minimum_value, maximum_value + 1)
+if quantity_tickets and numbers_by_ticket and minimum_value and maximum_value:
+	print("Processing tickets...")
+	list_of_tickets = tickets(quantity_tickets, numbers_by_ticket, minimum_value, maximum_value + 1)
+else:
+	print("Error! The requested tickets were not processed.")
+	list_of_tickets = []
 
 cards = ["Jack", "Queen", "King", "Ace"]
 print()
