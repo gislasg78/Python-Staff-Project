069c805329f85b5c9233f9c9f3bc169f8c3d2cbc
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2026-02-24 10:11:03 -0600

    Mathematical Functions General Project
    Programs that implement different exercises in Python to practice data structures and their respective instructions.

--- IndexList.py
+++ IndexList.py
@@ -16,10 +16,28 @@ print("Search for an item in a list.")
 item = int(input("Enter the item to search for: "))
 
 list_search = [[2, 2, 2], 3, 5, 5, [2, 2, 3], 3, 5, 5, [7, 7, 3], 3, 2, 2]
-list_search = [[[1, 2, 3], 2, [1, 3]], [1, 2, 3]]
 list_obtained = index_all(list_search, item)
 
 print("")
-print(f"Item searched:\t\t[{item}].")
-print(f"Nested search list:\t{list_search}.")
-print(f"Positions ​​obtained:\t{list_obtained}.")
+print("Results with the positions of the element searched.")
+print(f"+ Item searched:\t[{item}].")
+print(f"+ Nested search list:\t{list_search}.")
+print(f"+ Positions ​​obtained:\t{list_obtained}.")
+
+list_search = [[[1, 2, 3], 2, [1, 3]], [1, 2, 3], [3, 2, 1], [2, 3, 1]]
+list_obtained = index_all(list_search, item)
+
+print()
+print("Results with the positions of the element searched.")
+print(f"+ Item searched:\t[{item}].")
+print(f"+ Nested search list:\t{list_search}.")
+print(f"+ Positions ​​obtained:\t{list_obtained}.")
+
+list_search = [[[1, 2, 3], 2, [1, 3]], [1, 2, 3]]
+list_obtained = index_all(list_search, item)
+
+print()
+print("Results with the positions of the element searched.")
+print(f"+ Item searched:\t[{item}].")
+print(f"+ Nested search list:\t{list_search}.")
+print(f"+ Positions ​​obtained:\t{list_obtained}.")
