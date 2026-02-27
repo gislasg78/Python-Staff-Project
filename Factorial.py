c65bf68402ee30b3ff04cbeecf13a545c538b831
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2026-02-26 18:41:31 -0600

    Mathematical Functions General Project
    The primary purpose of this program is to calculate the factorial of any integer limited to a maximum number of system recursions.

--- Factorial.py
+++ Factorial.py
@@ -1,5 +1,12 @@
 import sys
 
+class Exception_Negative_Numbers(Exception):
+	def __init__(self, message):
+		self.message = message
+
+	def __str__(self):
+		return f"Exception for Negative Numbers: [{self.message}]."
+
 def factorial(number):
 	if number <= 1:
 		return 1
@@ -11,17 +18,18 @@ print("Obtaining the factorial of a number.")
 
 try:
 	number = int(input("Enter an integer: "))
+	factorial_number = factorial(number)
 
 	if number > 0:
-		factorial_num = factorial(number)
-
 		print()
 		print("Results.")
 		print(f"+ Number\t: [{number}].")
-		print(f"+ Factorial\t: [{factorial_num}].")
+		print(f"+ Factorial\t: [{factorial_number}].")
 		print(f"+ Recursions\t: [{sys.getrecursionlimit()}].")
 	elif number < 0:
-		raise Exception("Negative numbers should not be entered.")
+		raise Exception_Negative_Numbers("Negative numbers should not be entered")
+	else:
+		raise Exception(f"The factorial for the number: [{number}] will always be: [{factorial_number}].")
 
 except Exception as e:
 	print()
